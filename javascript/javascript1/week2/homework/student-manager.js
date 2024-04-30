b22be48f95b4f570f03b76e24f71bd49c5566211
tonevrav <tonevrav@gmail.com>
2024-04-30 10:32:19 +0200

    Have added testings

--- javascript/javascript1/week2/homework/student-manager.js
+++ javascript/javascript1/week2/homework/student-manager.js
@@ -3,7 +3,6 @@ const class07Students = [];
 function addStudentToClass(studentName) {
 
     if(studentName === "Queen") {
-        console.log("We are adding Queen to a class!");
         class07Students.push(studentName);
     } else {
         if(class07Students.includes(studentName)) {
@@ -23,3 +22,18 @@ function addStudentToClass(studentName) {
 function getNumberOfStudents() {
     return class07Students.length;
 }
+
+// Add some students to the class.
+// Add more students than there is space for
+// Add a student that is already in the class
+// Add the Queen to a full class
+// Call the getNumberOfStudents to log out the number of students.
+
+addStudentToClass("Poly");
+addStudentToClass("Lory");
+addStudentToClass("Dory");
+addStudentToClass("Zari");
+addStudentToClass("Vali");
+addStudentToClass("Bari");
+addStudentToClass("Fari");
+addStudentToClass("Queen");
