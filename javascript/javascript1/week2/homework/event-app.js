77f5c653bbc12801a95e1e90affa863e1461ca03
tonevrav <tonevrav@gmail.com>
2024-05-04 09:34:50 +0200

    Optimized code by updating the function

--- javascript/javascript1/week2/homework/event-app.js
+++ javascript/javascript1/week2/homework/event-app.js
@@ -1,19 +1,10 @@
-function getEventWeekday(numbersOfDays) { // 15
+function getEventWeekday(numbersOfDays) {
 
-    const currentDay = new Date().getDay(); // e.g. 6 (Saturday)
+    const currentDay = new Date().getDay();
 
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const remainingDays = numbersOfDays % 7; // 15 % 7 = 1
 
-    let indexOfEventDay = currentDay + remainingDays; // 6 + 1 = 7
+    const indexOfEventDay = (currentDay + numbersOfDays) % 7;
 
-    if(indexOfEventDay >= 7) { // true
-    indexOfEventDay -= 7; // 7 - 7 = 0
-    }
-
-    return days[indexOfEventDay]; // days[0] (Sunday)
+    return days[indexOfEventDay];
 }
-
-console.log(getEventWeekday(1));
-console.log(getEventWeekday(3));
-console.log(getEventWeekday(14));
