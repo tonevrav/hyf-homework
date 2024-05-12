fb266130a288d33bd3a074cf6898a450e513893a
tonevrav <tonevrav@gmail.com>
2024-05-12 10:37:42 +0200

    Implemented the recommendations after the review. Optimised the code

--- javascript/javascript1/week3/homework/remove-item-from-array.js
+++ javascript/javascript1/week3/homework/remove-item-from-array.js
@@ -9,21 +9,21 @@ const names = [
     "katrine",
     "Tala",
   ];
-  const nameToRemove = "aHMaD";
 
   // LOGIC
   // Write some code here
   function deleteItemFromArray(array, itemToDelete) {
-
-    for (let index = 0; index < array.length; index++) {
-        if (array[index].toLowerCase() === itemToDelete.toLowerCase()) {
-            array.splice(index, 1);
-            break;
-        }
+    const indexOfNameToRemove = names.indexOf(nameToRemove);
+    if (indexOfNameToRemove >= 0) {
+      names.splice(indexOfNameToRemove, 1);
+    } else {
+      console.log("Sorry, there is no person in the list with that name. Please, make sure that you've typed the name correctly");
     }
   }
   // RENDER
   // Code done
   
+  const nameToRemove = "aHMaD";
+  // const nameToRemove = "Ahmad";
   deleteItemFromArray(names, nameToRemove);
   console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
